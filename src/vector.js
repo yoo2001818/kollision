80b308d5978dcf09b3cf197f9d87979fff808465
Duknam Yoo <yoo2001818@gmail.com>
2016-03-18 15:44:48 +0900

    Reimplement vector class using Float32Array

--- src/vector.js
+++ src/vector.js
@@ -1,105 +1,66 @@
-export default class Vector {
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
+const vector = {
+  create(x, y) {
+    let target = new Float32Array(2);
+    target[0] = x;
+    target[1] = y;
+    return target;
+  },
+  set(x, y, dest) {
+    dest[0] = x;
+    dest[1] = y;
+    return dest;
+  },
+  copy(target, dest) {
+    dest[0] = target[0];
+    dest[1] = target[1];
+    return dest;
+  },
+  add(a, b, dest) {
+    dest[0] = a[0] + b[0];
+    dest[1] = a[1] + b[1];
+    return dest;
+  },
+  addScalar(target, value, dest) {
+    dest[0] = value + target[0];
+    dest[1] = value + target[1];
+    return dest;
+  },
+  subtract(a, b, dest) {
+    dest[0] = a[0] - b[0];
+    dest[1] = a[1] - b[1];
+    return dest;
+  },
+  multiply(target, value, dest) {
+    dest[0] = value * target[0];
+    dest[1] = value * target[1];
+    return dest;
+  },
+  divide(target, value, dest) {
+    if (value === 0) return dest;
+    dest[0] = target[0] / value;
+    dest[1] = target[1] / value;
+    return dest;
+  },
+  invert(target, dest) {
+    dest[0] = -target[0];
+    dest[1] = -target[1];
+    return dest;
+  },
+  dot(a, b) {
+    return a[0] * b[0] + a[1] * b[1];
+  },
+  cross(a, b) {
+    return a[0] * b[1] - a[1] * b[0];
+  },
+  lengthSquared(target) {
+    return target[0] * target[0] + target[1] * target[1];
+  },
+  length(target) {
+    return Math.sqrt(vector.lengthSquared(target));
+  },
+  normalize(target, dest) {
+    return vector.divide(target, vector.length(target), dest);
   }
-  set(x, y) {
-    if (x instanceof Vector) {
-      return this.setVector(x);
-    }
-    this.x = x;
-    this.y = y;
-    return this;
-  }
-  setVector(target) {
-    this.x = target.x;
-    this.y = target.y;
-    return this;
-  }
-  add(x, y) {
-    if (x instanceof Vector) {
-      return this.addValue(x);
-    }
-    this.x += x;
-    this.y += y;
-    return this;
-  }
-  addVector(target) {
-    this.x += target.x;
-    this.y += target.y;
-    return this;
-  }
-  addScalar(value) {
-    this.x += value;
-    this.y += value;
-    return this;
-  }
-  subtract(x, y) {
-    if (x instanceof Vector) {
-      return this.subtractVector(x);
-    }
-    this.x -= x;
-    this.y -= y;
-    return this;
-  }
-  subtractVector(target) {
-    this.x -= target.x;
-    this.y -= target.y;
-    return this;
-  }
-  multiply(value) {
-    this.x *= value;
-    this.y *= value;
-    return this;
-  }
-  divide(value) {
-    // Filter 0, since 0 will cause a problem
-    if (value === 0) return this;
-    this.x /= value;
-    this.y /= value;
-    return this;
-  }
-  invert() {
-    this.x -= this.x;
-    this.y -= this.y;
-    return this;
-  }
-  dot(target) {
-    return this.x * target.x + this.y * target.y;
-  }
-  cross(target) {
-    return this.x * target.y - this.y * target.x;
-  }
-  lengthSquared() {
-    return this.x * this.x + this.y * this.y;
-  }
-  length() {
-    return Math.sqrt(this.lengthSquared());
-  }
-  setLength(l) {
-    return this.multiply(l / this.length());
-  }
-  normalize() {
-    return this.divide(this.length());
-  }
-  angle() {
-    return Math.atan2(this.y, this.x);
-  }
-  distanceSquared(target) {
-    let distX = target.x - this.x;
-    let distY = target.y - this.y;
-    return distX * distX + distY * distY;
-  }
-  distance(target) {
-    return Math.sqrt(this.distanceSquared(target));
-  }
-  equals(target) {
-    return this.x === target.x && this.y === target.y;
-  }
-  clone() {
-    return new Vector(this.x, this.y);
-  }
-  translate(target) {
-    return this.add(target);
-  }
-}
+};
+
+export default vector;
